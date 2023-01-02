9f742869164cdd56fb16baa60c7617de79cda67c
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2023-01-02 12:38:22 -0300

    Testando estilização do button e TextField do Material UI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
-import { Button } from "@mui/material";
-
+import { Button, TextField } from "@mui/material";
 
 function App() {
   return (
     <div>
       <h2>Hello, World!</h2>
-      <Button variant="contained">Start</Button>
+      <div>
+        <TextField style={{ margin: '15px 5px' }} id="outlined-basic" label="E-mail" variant="outlined" />
+
+      </div>
+        
+        <Button style={{ margin: '15px 5px' }} variant="contained">Enviar</Button>
 
     </div>
   );
